2f0571439b48782b6c9eab1e5cae9a0a6596d10a
Pratibhabhagat <41480302+Pratibhabhagat@users.noreply.github.com>
2021-06-07 18:58:13 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,35 @@
+
+package com.cg.demo;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please Enter the integer value- ");
+		int num = sc.nextInt();
+		System.out.println(num);
+		
+		System.out.println("Please Enter the integer value- ");
+		double num2 = sc.nextDouble();
+		System.out.println(num2);
+		
+		System.out.println("Please Enter the integer value- ");
+		String name = sc.next();
+		System.out.println(name);
+			}
+//
+//   public static void main(String[] args) {
+//	   int       num            =                 100           ;
+////  (keyword)   (identifiers)   (operators)       (Literal)     (seperator)
+//	   
+//	   System.out.println(num);
+//}
+//
+//
+  }
+
+
+ 
